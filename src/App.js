8a03484789989bfacd69c655bc01c841f2a465c7
Datkaiym <datuakylbekova@gmail.com>
2022-10-12 14:02:53 +0600

    add authorization functionality

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Проект: <b> TO DO-APP</b>
+        <p>
+          Разработчик №1: <b>Джонни</b>
+        </p>
+      </p>
+    </div>
+  );
+}
+
+export default App;
